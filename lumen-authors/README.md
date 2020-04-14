ba71d343957b0b6b389c016e0cbd66cf159af7ee
techguy <jobvinny@1993>
2020-04-14 04:39:33 +0300

    :construction: lumen authors

---
+++ lumen-authors/README.md
@@ -0,0 +1,3 @@
+# Lumen PHP Framework
+
+# <p class="text-center"><img src="https://webkul.com/wp-content/uploads/2020/01/laravel-development-banner.png"></p>
---
+++ lumen-authors/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Traits\APIResponser;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+
+class AuthorController extends Controller
+{
+    use APIResponser;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        return $this->sucessResponse(Author::query()->latest()->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'max:255'],
+            'gender' => ['required', 'max:255', 'in:male,female'],
+            'country' => ['required', 'max:255'],
+        ]);
+
+        return $this->sucessResponse(Author::query()->create($request->all()), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show($id)
+    {
+        return $this->sucessResponse(Author::query()->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => ['max:255'],
+            'gender' => ['max:255', 'in:male,female'],
+            'country' => ['max:255'],
+        ]);
+
+        $author = Author::query()->findOrFail($id);
+        $author->fill($request->all());
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return $this->sucessResponse($author);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function destroy($id)
+    {
+        $author = Author::query()->findOrFail($id);
+        $author->delete();
+        return $this->sucessResponse($author);
+    }
+}
